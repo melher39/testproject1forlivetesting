77d80a14ae5c77f8514c4fac9f047943d3f41bb7
Melvin Hernandez <melher39@gmail.com>
2018-11-20 20:33:53 -0600

    added html and js file. live test 1

---
+++ assets/ytlogic2.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+
+
+    // this will be where we connect with the youtube api
+    // just testing to see if it works on github
+    
+    console.log("12 12 12");
+    
+    
+    function searchYouTube(){
+    
+        var searchTerm = "chicken alfredo";
+        // var limit = 5;
+        
+        var apiKEY = "&key=AIzaSyDpWKvpsuH5kB8LRiI2KFGL3DdqwW5aC7M"
+    
+        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + searchTerm
+        + apiKEY;
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+    
+            var results = response.items;
+    
+            for(var i=0; i< results.length; i++) {
+            console.log(results);
+    
+            
+    
+            var video = results[i].snippet.thumbnails.default.url;
+    
+            var resultsDisplayed = $("<img>").attr("src", video);
+    
+            resultsDisplayed.attr("data-id", results[i].id.videoId);
+    
+            resultsDisplayed.attr("data-title", results[i].snippet.title);
+    
+            resultsDisplayed.append(video);
+    
+            $("#video-space").append("<br>",resultsDisplayed);
+            }
+    
+    
+        });
+    
+    };
+    
+    $("#submit-button").on("click", function(){
+            
+        event.preventDefault();
+    
+        alert("click");
+        console.log("click click");
+        searchYouTube();
+    });
+    
+    function selectVideo(){
+        var imgTitle = $(this).attr("data-title");
+    
+        var imgId = $(this).attr("data-id");
+    
+        var player = $("<iframe id='ytplayer' type='text/html' width='640' height='360' src='https://www.youtube.com/embed/'" + imgId + " frameborder='0'></iframe>");
+    
+        $("#video-space").hide();
+    
+        $("#left-space").append(player);
+    
+        
+    
+    
+    
+        console.log(imgTitle);
+    
+        console.log(imgId);
+    
+    };
+    
+    
+    // var player;
+    
+    // function onYouTubeIframeAPIReady() {
+    //   player = new YT.Player('player', {
+    //     height: '390',
+    //     width: '640',
+    //     videoId: 'M7lc1UVf-VE',
+    //     events: {
+    //       'onReady': onPlayerReady,
+    //       'onStateChange': onPlayerStateChange
+    //     }
+    //   });
+    // }
+    
+    
+    
+    $(document).on("click", "img", selectVideo);
+    
+    
+    
+    
+    
+    });
